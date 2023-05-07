233c8a9c141f649ef6bfb1164af6498ee8b6efe5
5an7y <jose.santiago7@hotmail.com>
2023-05-07 12:27:02 -0600

    Change generator streams

--- Examples/Template/case_generator.cpp
+++ Examples/Template/case_generator.cpp
@@ -3,6 +3,11 @@
 
 int main(int argc, char *argv[]) {
     Generator::init_generator(argc, argv);    
+    /*
+        Use Generator::case_in<< to put elements in case input.
+        Use Generator::case_out<< to put elements in case output.
+        Use Generator::arguments>> to read the arguments.
+    */
 
     return 0;
 }
--- Libs/Generator.hpp
+++ Libs/Generator.hpp
@@ -4,22 +4,36 @@
 #include <exception>
 #include <string>
 #include <sstream>
+#include <fstream>
 
 namespace Generator {
+    std::ofstream case_in, case_out;
+    std::stringstream arguments;
+
     void init_generator(int argc, char *argv[]) {
         if (argc <= 2) 
             throw std::invalid_argument("Missing requiered argument \"case_name\"");
         
-        freopen(argv[1], "w", stdout);
+        std::string case_in_name(argv[1]), case_out_name(argv[1]);
+        case_in_name += ".in"; case_out_name += ".out";
+
+        case_in.open(case_in_name.c_str());
+        case_out.open(case_out_name.c_str());
+
+        if (!case_in) {
+            throw std::runtime_error("Error al abrir " + case_in_name + "\n");
+            exit(EXIT_FAILURE);
+        }
+        if (!case_out) {
+            throw std::runtime_error("Error al abrir " + case_out_name + "\n");
+            exit(EXIT_FAILURE);
+        }
         
         if (argc > 2) {
-            std::string S(argv[2]);
-            for (int i = 3; i < argc; i++) {
+            for (int i = 2; i < argc; i++) {
                 std::string B(argv[i]);
-                S = S + " " + B;
+                arguments << B << " \n"[i == argc-1];
             }
-            std::istringstream buf(S);
-            std::cin.rdbuf(buf.rdbuf());
         }
 
         return;
