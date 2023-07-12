d561b86fc318bbd0121a95312191cf902343658c
5an7y <jose.santiago7@hotmail.com>
2023-07-11 23:12:02 -0700

    Constants Library

---
+++ Libs/Constants.hpp
@@ -0,0 +1,10 @@
+#pragma once
+
+#include <string>
+
+namespace Constants {
+    const std::string lowercaseEnglishAlphabet = "abcdefghijklmnopqrstuvwxyz",
+                      uppercaseEnglishAlphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+                      specialCharacters = "~`!@\"{}[]()*&^%$#_+=-|?/\\.,",
+                      spaces = " \n";
+};
